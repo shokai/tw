5afedbbc47e304129757bb1921e56881d40ff4ee
Sho Hashimoto <hashimoto@shokai.org>
2012-10-18 02:10:37 +0900

    add test of t.co short url #30

---
+++ test/test_t_co.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestTCo < Test::Unit::TestCase
+
+  def setup
+    @client = Tw::Client.new
+    @client.auth
+  end
+
+  def test_t_co_char_length
+    msg = "blog書いた → http://shokai.org/blog/archives/6513"
+    assert msg.char_length > msg.char_length_with_t_co
+  end
+end
