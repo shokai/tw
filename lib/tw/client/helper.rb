9e09935993295ee60351f36b7ee9a108f2dca440
Sho Hashimoto <hashimoto@shokai.org>
2013-01-04 08:01:00 +0900

    fix

--- lib/tw/client/helper.rb
+++ lib/tw/client/helper.rb
@@ -9,7 +9,6 @@ class String
     Tw::Conf.update_twitter_config
     len = self.char_length
     self.scan(Regexp.new "https?://[^\s]+").each do |url|
-      puts url
       len += (url =~ /^https/ ? Tw::Conf['twitter_config']['short_url_length_https'] : Tw::Conf['twitter_config']['short_url_length']) - url.char_length
     end
     len
