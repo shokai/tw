510a95f29b2c6842f104036e94f45d7f08948364
Sho Hashimoto <hashimoto@shokai.org>
2014-01-28 23:18:50 +0900

    update for rainbow gem v2.0.0  #60

--- lib/tw/app/helper.rb
+++ lib/tw/app/helper.rb
@@ -3,7 +3,7 @@ class String
   def colorize(pattern)
     self.split(/(#{pattern})/).map{|term|
       if term =~ /#{pattern}/
-        term = term.color(Tw::App::Render.color_code term).bright.underline
+        term = Rainbow(term).color(Tw::App::Render.color_code term).bright.underline
       end
       term
     }.join('')
